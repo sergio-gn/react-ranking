56029d837ba73770d62a5dfa670380bc9922cbe1
Sergio Garcia Neto <88346250+sergio-gn@users.noreply.github.com>
2023-06-23 20:13:40 +1000

    Update MultiCard.jsx

--- src/MultiCard.jsx
+++ src/MultiCard.jsx
@@ -1,52 +1,86 @@
-    import React, { useState } from 'react';
+import React, { useState } from 'react';
 
-    const Card = ({ name }) => {
-    const [count, setCount] = useState(0);
-    const [upvoted, setUpvoted] = useState(false);
-    const [downvoted, setDownvoted] = useState(false);
+const Card = ({ name, count, handleUpvote, handleDownvote }) => {
+  const [upvoted, setUpvoted] = useState(false);
+  const [downvoted, setDownvoted] = useState(false);
 
-    const handleUpvote = () => {
-        if (!upvoted) {
-        setCount(count + 1);
-        setUpvoted(true);
-        setDownvoted(false);
-        }
-    };
+  const handleUpvoteClick = () => {
+    handleUpvote();
+    setUpvoted(true);
+    setDownvoted(false);
+  };
 
-    
-    const handleDownvote = () => {
-        if (!downvoted) {
-        setCount(count - 1);
-        setDownvoted(true);
-        setUpvoted(false);
-        }
-    };
+  const handleDownvoteClick = () => {
+    handleDownvote();
+    setDownvoted(true);
+    setUpvoted(false);
+  };
 
-    return (
-        <div className="card">
-        <h2>{name}</h2>
-        <p>Counter: {count}</p>
-        <div className="buttons">
-            <button disabled={upvoted} onClick={handleUpvote}>
-            Upvote
-            </button>
-            <button disabled={downvoted} onClick={handleDownvote}>
-            Downvote
-            </button>
-        </div>
-        </div>
-    );
-    };
+  return (
+    <div className="card">
+      <h2>{name}</h2>
+      <p>Counter: {count}</p>
+      <div className="buttons">
+        <button disabled={upvoted} onClick={handleUpvoteClick}>
+          Upvote
+        </button>
+        <button disabled={downvoted} onClick={handleDownvoteClick}>
+          Downvote
+        </button>
+      </div>
+    </div>
+  );
+};
 
-    const CardList = () => {
-    const items = ['Dorothy', 'Darling', 'Dewling'];
-    return (
-        <div className="card-list">
-        {items.map((item) => (
-            <Card key={item} name={item} />
-        ))}
-        </div>
-    );
-    };
+const CardList = () => {
+  const [items, setItems] = useState([
+    { name: 'Dorothy', count: 0 },
+    { name: 'Darling', count: 0 },
+    { name: 'Dewling', count: 0 }
+  ]);
 
-    export default CardList;
+  const handleUpvote = (index) => {
+    setItems((prevItems) => {
+      const updatedItems = [...prevItems];
+      updatedItems[index] = {
+        ...prevItems[index],
+        count: prevItems[index].count + 1
+      };
+      return sortItems(updatedItems);
+    });
+  };
+
+  const handleDownvote = (index) => {
+    setItems((prevItems) => {
+      const updatedItems = [...prevItems];
+      if (prevItems[index].count > 0) {
+        updatedItems[index] = {
+          ...prevItems[index],
+          count: prevItems[index].count - 1
+        };
+        return sortItems(updatedItems);
+      }
+      return prevItems;
+    });
+  };
+
+  const sortItems = (items) => {
+    return items.sort((a, b) => b.count - a.count);
+  };
+
+  return (
+    <div className="card-list">
+      {items.map((item, index) => (
+        <Card
+          key={item.name}
+          name={item.name}
+          count={item.count}
+          handleUpvote={() => handleUpvote(index)}
+          handleDownvote={() => handleDownvote(index)}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default CardList;
